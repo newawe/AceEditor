50b34ed7a83f562aaa3c7dea60bf43d8d5fbb6da
Ron Friedman <ron852719@icloud.com>
2016-06-27 16:25:36 -0700

    Update share.php

--- share.php
+++ share.php
@@ -1,1 +1,25 @@
 <?php
+$code = $_POST['code'];
+$lang = $_POST['lang'];
+$name = "test"; //TODO: Replace with actual code
+$desc = "test"; //TODO: Replace with actual code
+$user = "test"; //TODO: Replace with actual code
+
+
+
+
+$servername = "localhost";
+$username = "BMS_id";
+$password = "Password1";
+$dbname = "BMS_id";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$sql = "INSERT INTO `BMS_id`.`Newawe_Project_Test` (`Code`, `ID`, `User`, `Lang`, `Name`, `Description`) VALUES (\'$code\', NULL, \'$user\', \'$lang\', \'$name\', \'$desc\');";
+mysqli_query($conn, $sql);
+
+
+//Redirect user back
+header("Location: ./index.html?code=$code");
